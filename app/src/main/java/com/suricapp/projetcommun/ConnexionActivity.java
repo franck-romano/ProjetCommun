1f121b59e202c2b466ac0dca4e74134c258f16c4
MaxenceMax <maxence.montet01@gmail.com>
2015-03-18 10:10:05 +0100

    mise à jour nom package et nom des classes

---
+++ app/src/main/java/com/suricapp/projetcommun/ConnexionActivity.java
@@ -0,0 +1,51 @@
+package com.suricapp.projetcommun;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class ConnexionActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connexion);
+
+        TextView inscription = (TextView)findViewById(R.id.inscription);
+        TextView oubli = (TextView)findViewById(R.id.oubli);
+        final Button connexion = (Button)findViewById(R.id.connexion);
+
+            connexion.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    TextView id = (TextView)findViewById(R.id.identifiant);
+                    TextView mdp = (TextView)findViewById(R.id.mdp);
+                    Intent intent = new Intent(ConnexionActivity.this,TimelineActivity.class);
+                    String ident = id.getText().toString();
+                    String mot = mdp.getText().toString();
+
+                    if(ident.trim().length()==0)
+                        Toast.makeText(getApplicationContext(),"Veuillez remplir les champs identifiant et mot de passe svp", Toast.LENGTH_SHORT).show();
+                    else {
+                        if ((ident.equals("toto")&&(mot.equals("toto"))))
+                                startActivity(intent);
+                        else
+                            Toast.makeText(getApplicationContext(), "Identifiant ou MDP Incorrect", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        inscription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ConnexionActivity.this, InscriptionActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/suricapp/projetcommun/InscriptionActivity.java
@@ -0,0 +1,75 @@
+package com.suricapp.projetcommun;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+
+public class InscriptionActivity extends ActionBarActivity {
+
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private TextView dateView;
+    private int year, month, day;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inscription);
+        dateView = (TextView) findViewById(R.id.date);
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month+1, day);
+
+        final Button bouton_envoyer = (Button)findViewById(R.id.suivant);
+
+        bouton_envoyer.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InscriptionActivity.this, Inscription_2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        // TODO Auto-generated method stub
+        if (id == 999) {
+            return new DatePickerDialog(this, myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener
+            = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
+            // arg1 = year
+            // arg2 = month
+            // arg3 = day
+            showDate(arg1, arg2+1, arg3);
+        }
+    };
+
+    private void showDate(int year, int month, int day) {
+        dateView.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
+    }
+}
